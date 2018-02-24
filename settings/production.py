2984d6aada939372eb14be4856c8baa2b99e9513
Shea Phillips <shea.phillips@gmail.com>
2018-02-23 21:32:26 -0800

    - adding production config file.

---
+++ settings/production.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taiga',
+        'USER': 'taiga',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_SERVICE_NAME', ''),
+        'PORT': '5432',
+    }
+}
